a4704fb299d94eed3a3dc0a11e9ff03362d638dd
Mbangura601 <mbangura601@gmail.com>
2022-04-07 16:44:37 -0700

    Task 4 hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stuio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+puts("\"Programming is likebuilding a multilingual puzzle");
+return (0);
+}
